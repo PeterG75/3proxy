76d644dc03ab60c1ba6c441436c0916d388ae957
Hidden Refuge <hidden-refuge@users.noreply.github.com>
2017-05-16 09:57:33 +0200

    Updated to 1.1
    - Updated 3proxy source code version
    - added comments to installation steps
    - updated reference links to files to github repo links

--- 3proxy-install.sh
+++ 3proxy-install.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
-###############################################
-# Automatic compilation of 3proxy from source #
-# http://freevps.us/thread-15471.html         #
-###############################################
+#######################################
+# 3proxy Semi Automatic Installer     #
+# with Authentication + Sample Config #
+# Version: 1.1                        #
+# Branch: Stable                      #
+#######################################
+# Author: Hidden Refuge               #
+# License: MIT (DO WHAT YOU WANT)     #
+# Copyright (c) 2017 - Hidden Refuge  #
+#######################################
 
+# Creating temporary working directory
 mkdir /tmp/proxy
 cd /tmp/proxy
-wget https://github.com/z3APA3A/3proxy/archive/0.8.8.tar.gz
-tar -xvzf 3proxy-0.8.8.tar.gz
+
+# Downloading latest stable 3proxy 0.8.9
+wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.9.tar.gz
+
+# Extracting archive
+tar -xvzf 3proxy-0.8.9.tar.gz
+
+# Compiling
 cd 3proxy/
 make -f Makefile.Linux
+
+# Moving executable file to current user home directory
 cd src/
 mv 3proxy $HOME
+
+# Downloading sample configuration to current user home directory
 cd $HOME
-wget http://kokakukidotai.insomnia247.nl/3proxy/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/hidden-refuge/3proxy/master/3proxy.cfg
+
+# Fixing permission on files
 chmod +x 3proxy
+
+# Removing temporary working directory from tmp
 rm -rf /tmp/proxy
+
+# Creating folder for logs or otherwise 3proxy won't work
 mkdir /var/log/3proxy
